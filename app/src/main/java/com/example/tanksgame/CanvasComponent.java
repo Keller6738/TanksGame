269f87a49e91223a6ec06987b044e480ea7c59f1
Keller6738 <giladke123@gmail.com>
2024-10-30 21:07:24 +0200

    updated class and made it inheritor of View.java class

--- app/src/main/java/com/example/tanksgame/CanvasComponent.java
+++ app/src/main/java/com/example/tanksgame/CanvasComponent.java
@@ -1,31 +1,57 @@
 package com.example.tanksgame;
 
-public abstract class CanvasComponent {
-    protected final Color kColor;
+import android.content.Context;
+import android.graphics.Canvas;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.AttributeSet;
+import android.view.View;
+
+public abstract class CanvasComponent extends View {
+    protected Color kColor;
     protected double x, y;
     protected int angle;
-    protected double TIME = 0.05;
 
-    public CanvasComponent(Color kColor, double x, double y, int angle) {
-        this.kColor = kColor;
-        this.x = x;
-        this.y = y;
-        this.angle = angle;
+    protected Handler handler;
+
+    public CanvasComponent(Context context, AttributeSet attrs) {
+        super(context, attrs);
+
+        angle = 0;
+
+        handler = new Handler(Looper.getMainLooper());
     }
 
     public Color getColor() {
         return kColor;
     }
 
-    public void move() {
-        this.x += 2 * Math.cos(angle);
-        this.y += 2 * Math.sin(angle);
+    protected void move() {
+        // Convert the angle to radians for use with Math functions
+        double angleInRadians = Math.toRadians(angle);
+
+        // Calculate movement based purely on angle
+        this.x += 10 * Math.cos(angleInRadians);
+        this.y += 10 * Math.sin(angleInRadians);
     }
 
-    public void turn() {
-        this.angle += 1;
+    protected void setColor(Color color) {
+        kColor = color;
     }
 
-    abstract void dutyCycle();
-    abstract void draw();
+    public void setInitX(double x) {
+        this.x = x;
+    }
+
+    public void setInitY(double y) {
+        this.y = y;
+    }
+
+    protected void dutyCycle(Runnable runnable) {
+        handler.post(runnable);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+    }
 }
